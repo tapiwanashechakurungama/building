ddbab618ca811c796907e14aaffe21f6fc1f0ac9
tapiwanashechakurungama <chakurungamatapiwanashe@gmail.com>
2026-04-27 16:12:17 +0200

    updates

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -57,31 +57,19 @@ const Footer = () => {
           <span>Engineered in concrete & steel</span>
         </div>
         <br />
-         <div className="border-t border-background/10 mt-20 pt-8 flex flex-col md:flex-row justify-between gap-4 font-mono text-[11px] text-background/50 uppercase tracking-[0.15em]">
+         <div className="border-t border-background/10  pt-4 flex flex-col md:flex-row justify-between gap-4 font-mono text-[11px] text-background/50 uppercase tracking-[0.15em]">
           <span>Developer</span>
           <a href="https://wa.me/263733517788" 
          target="_blank" 
          rel="noopener noreferrer"
          class="
-           group
-           inline-flex 
-           items-center 
-           justify-center 
-           gap-3 
+           
            bg-[#25D366] 
            hover:bg-[#20B359] 
            text-white 
            font-bold 
-           text-lg 
-           md:text-xl 
-           py-1.5 
-           px-5 
-           md:py-4 
-           md:px-10 
-           rounded-2xl 
-           shadow-lg 
-           hover:shadow-xl 
-           transform 
+          
+          
            hover:scale-[1.02] 
            active:scale-[0.98]
            transition-all 
@@ -93,20 +81,8 @@ const Footer = () => {
            sm:w-auto
            cursor-pointer
          "
-         aria-label="Chat with Neichnice Technologies on WhatsApp">
-        
-
-        <span class="flex items-center justify-center w-6 h-6 md:w-7 md:h-7">
-          <svg class="w-full h-full fill-current text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.032 2.001c-5.516 0-10 4.484-10 10 0 1.762.456 3.468 1.322 4.98L2.03 22.27l5.41-1.436c1.45.791 3.099 1.227 4.827 1.227 5.516 0 10-4.484 10-10s-4.484-10-10-10zm0 18.378c-1.575 0-3.09-.427-4.42-1.224l-.317-.188-3.212.852.857-3.138-.205-.328a7.393 7.393 0 0 1-1.176-3.974c0-4.074 3.315-7.389 7.389-7.389s7.389 3.315 7.389 7.389-3.315 7.389-7.389 7.389zm4.111-5.518c-.225-.113-1.334-.659-1.541-.734-.207-.075-.357-.113-.507.113-.15.226-.584.734-.716.885-.132.151-.264.169-.489.056-.225-.113-.951-.35-1.812-1.118-.67-.597-1.122-1.334-1.254-1.559-.132-.226-.014-.348.099-.46.103-.101.226-.264.339-.396.113-.132.15-.226.226-.377.075-.151.038-.283-.019-.396-.056-.113-.507-1.223-.694-1.674-.182-.44-.367-.38-.507-.387-.13-.006-.283-.007-.435-.007-.151 0-.396.057-.603.283-.207.226-.792.774-.792 1.887 0 1.113.81 2.188.923 2.339.113.151 1.595 2.435 3.864 3.415.54.233.961.372 1.291.477.542.173 1.036.149 1.426.09.435-.065 1.334-.545 1.521-1.072.188-.526.188-.977.132-1.071-.056-.094-.207-.151-.432-.264z"/>
-          </svg>
-        </span>
-        
-       
+         aria-label="Chat with Neichnice Technologies on WhatsApp">  
         <span class="tracking-wide">Neichnice</span>
-        
-      
-        
       </a>
         </div>
 
